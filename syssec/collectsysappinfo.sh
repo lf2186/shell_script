1ce7389735458866d41b73eb0b083a830074eda5
lf2186 <linfei3389@qq.com>
2014-12-30 10:37:29 +0800

    201412301036

--- syssec/collectsysappinfo.sh
+++ syssec/collectsysappinfo.sh
@@ -1,15 +1,61 @@
 #!/bin/sh
 
-
+LSB=$(which lsb_release)
+HNAME=$(which hostname)
+UNAME=$(which uname)
 
 
 function get_hostname(){
-	echo "Hostname : $(hostname -s)"
+	[ -x $HNAME ] && echo "$(hostname -s)" || echo "$HNAME command is not installed, get nothing"
+}
+
+hostname=$(get_hostname) || exit 1
+
+function get_osinfo(){
+	[ -x $LSB ] && echo "$hostname : Os : $(lsb_release -ds)" || echo "$LSB command is not installed, get nothing"
+}
+
+function get_kernelinfo(){
+	[ -x $UNAME ] && echo "$hostname : Kernel : $(uname -r)" || echo "$UNAME command is not installed, get nothing"
+	[ -x $UNAME ] && echo "$hostname : Process : $(uname -p)" || echo "$UNAME command is not installed, get nothing"
+}
+
+function get_netinfo(){
+	[ -x $HNAME ] && echo "$hostname : ipaddr : $(hostname -I)" || echo "$HNAME command is not installed, get nothing"
+	#[ -r /etc/resolv.conf ] && echo $(sed -e '/^$/d' /etc/resolv.conf | awk '{if (tolower($1)=="nameserver") print $2}' | sed "s/^/$hostname : dnsserver : /g")
+	[ -r /etc/resolv.conf ] && local dnsip=$(sed -e '/^$/d' /etc/resolv.conf | awk '{if (tolower($1)=="nameserver") print $2}' | sed "s/^/$hostname : dnsserver : /g") && echo ${dnsip}
+	#echo "${dnsip}"
+}
+
+function get_startapplist(){
+	local dapps=$(ps -ef | grep -E 'nginx|java|php' | grep -v grep)
+	nginxsbin=$(echo ${dapps} | sed 's/.*master process \([0-9a-z\/]\{1,\}nginx\).*/\1/')
+	tomcatdir=$(echo ${dapps} | sed 's/.*Dcatalina.home=\([0-9a-z\/]\{1,\}\).*/\1/')
+	#nginx_version=$(nginx -v)
+	#echo "$hostname : nginx_version : $nginx_version"
+	echo "$hostname : tomcat : $tomcatdir"
+	#echo $nginx_version
+}
+
+get_startapplist
+
+#function get_nostartapplist(){
+#	ps -ef | grep -E 'nginx|java|' 
+#}
+
+function read_input() {
+	os=$(get_osinfo | awk '{print $5}')
+	case $os in
+		Ubuntu) get_osinfo
+				get_kernelinfo
+				get_netinfo
+		;;
+		Centos) echo nono ;;
+		*)
+			echo "system is not knowed"
+	esac
+
 
 }
 
-
-
-
-
-
+read_input
